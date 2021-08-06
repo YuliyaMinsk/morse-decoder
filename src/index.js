4895835a0fc76d8847120faa386aa8fb926eeaac
Yuliya <yuliya.rostova@gmail.com>
2021-08-07 01:08:27 +0300

    feat: add task functionality

--- src/index.js
+++ src/index.js
@@ -38,8 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let decodedString = '';
+    let morseString = '';
+
+    for (let i = 0; i < expr.length; (i += 10)) {
+        //console.log ('Substr[' + i + ']: ' + expr.substr(i, 10));
+        morseString = '';
+        for (let j = 0; j < 10; j +=2 ) {
+            if (expr.substr(i+j, 2) === '**') {morseString = morseString + ' '; break;}
+            if (expr.substr(i+j, 2) === '00') continue;
+            if (expr.substr(i+j, 2) === '10') morseString = morseString + '.';
+            else  if (expr.substr(i+j, 2) === '11') morseString = morseString + '-';
+        }
+        //console.log(MORSE_TABLE[morseString]);
+        if (morseString == ' ') decodedString = decodedString + ' ';
+        else decodedString = decodedString + MORSE_TABLE[morseString];
+    }
+    //console.log(decodedString);
+    return decodedString;
 }
+
+
 
 module.exports = {
     decode
